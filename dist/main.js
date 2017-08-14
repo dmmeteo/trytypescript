5ba0cdec956325142aa1d9a5d8850abc53bb15f8
Dmitry Sichkar <dmmeteo@gmail.com>
2017-08-15 01:43:01 +0300

    work with variables

--- dist/main.js
+++ dist/main.js
@@ -1,37 +1,14 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var SweetSweetBasil = (function () {
-    function SweetSweetBasil() {
+var hello = 'Hi!';
+var hi = 'Hello';
+hello = 123;
+hi = 'abcabc';
+function f(input) {
+    var a = 100;
+    if (input) {
+        var b = a + 1202;
+        return b;
     }
-    SweetSweetBasil.prototype.getColor = function () {
-        console.log(this.color);
-    };
-    return SweetSweetBasil;
-}());
-var world = new SweetSweetBasil();
-var Basil = (function (_super) {
-    __extends(Basil, _super);
-    function Basil(name, startColor) {
-        var _this = _super.call(this) || this;
-        _this.name = name;
-        _this.color = startColor;
-        return _this;
-    }
-    Basil.prototype.setNewColor = function (newColor) {
-        this.color = newColor;
-        _super.prototype.getColor.call(this);
-    };
-    return Basil;
-}(SweetSweetBasil));
-var basil3 = new Basil('basil', 'red');
-basil3.getColor();
-basil3.setNewColor('bright green');
-basil3.getColor();
+    return a;
+}
+console.log(f(true));
+console.log(f(false));
--- main.ts
+++ main.ts
@@ -1,33 +1,51 @@
-class SweetSweetBasil {
-    name: string;
-    color: string;
-    constructor(/*name:string, startColor:string*/) {
-        // console.log('Hello ' + name)
-        // this.color = startColor
-    }
-    getColor(){
-        console.log(this.color)
-    }
-}
-let world = new SweetSweetBasil() //'World!', 'green')
-// let basil = new SweetSweetBasil('Basil', 'blue')
-// console.log(world.color)
+let hello:any = 'Hi!'
+var hi = 'Hello'
 
-class Basil extends SweetSweetBasil {
-    constructor(name:string, startColor:string) {
-        // super(name, startColor)
-        super()
-        this.name = name
-        this.color = startColor
+hello = 123
+hi = 'abcabc'
+
+function f(input: boolean) {
+    let a = 100
+    if(input){
+        let b = a + 1202
+        return b
     }
-    setNewColor(newColor:string) {
-        this.color = newColor
-        super.getColor()
-    }
+    return a
 }
 
-let basil3 = new Basil('basil', 'red')
+console.log(f(true))
+console.log(f(false))
 
-basil3.getColor()
-basil3.setNewColor('bright green')
-basil3.getColor()
+// class SweetSweetBasil {
+//     name: string;
+//     color: string;
+//     constructor(/*name:string, startColor:string*/) {
+//         // console.log('Hello ' + name)
+//         // this.color = startColor
+//     }
+//     getColor(){
+//         console.log(this.color)
+//     }
+// }
+// let world = new SweetSweetBasil() //'World!', 'green')
+// // let basil = new SweetSweetBasil('Basil', 'blue')
+// // console.log(world.color)
+//
+// class Basil extends SweetSweetBasil {
+//     constructor(name:string, startColor:string) {
+//         // super(name, startColor)
+//         super()
+//         this.name = name
+//         this.color = startColor
+//     }
+//     setNewColor(newColor:string) {
+//         this.color = newColor
+//         super.getColor()
+//     }
+// }
+//
+// let basil3 = new Basil('basil', 'red')
+//
+// basil3.getColor()
+// basil3.setNewColor('bright green')
+// basil3.getColor()
